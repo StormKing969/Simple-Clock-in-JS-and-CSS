38a190d920ff2a22eca4978381d3a622d82476d8
StormKing969 <wijesinghesaj@gmail.com>
2022-03-27 01:17:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,29 +7,25 @@ function setDate() {
 
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-
-    if(secondsDegrees >= 444) {
+    if(secondsDegrees === 90) {
         secondHand.style.transition = `none`;
-    } else {
-        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     }
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-    if(secondsDegrees >= 444) {
-        minsHand.style.transition = `none`;
-    } else {
-        minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    if(minsDegrees === 90) {
+        minsDegrees.style.transition = `none`;
     }
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
     
 
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-    if(secondsDegrees >= 444) {
-        hourHand.style.transition = `none`;
-    } else {
-        hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    if(hourDegrees === 90) {
+        hourDegrees.style.transition = `none`;
     }
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
     
 }
 
